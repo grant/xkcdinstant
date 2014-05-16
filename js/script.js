aa6b39e17e97acc50c98929549a613214c64d2bd
Grant Timmerman <granttimmerman@gmail.com>
2014-05-16 16:26:51 -0700

    Add basic setup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 $(function () {
   console.log('search');
+  $(function () {
+    // var wall = new freewall('#container');
+
+    var temp = "<div class='brick' style='width:{width}px;'><img src='i/photo/{index}.jpg' width='100%'></div>";
+    var w = 1, h = 1, html = '', limitItem = 49;
+    for (var i = 0; i < limitItem; ++i) {
+      w = 1 + 3 * Math.random() << 0;
+      html += temp.replace(/\{width\}/g, w*150).replace("{index}", i + 1);
+    }
+    $("#container").html(html);
+
+    var wall = new freewall("#container");
+    wall.reset({
+      selector: '.brick',
+      animate: true,
+      cellW: 150,
+      cellH: 'auto',
+      onResize: function() {
+        wall.fitWidth();
+      }
+    });
+
+    var images = wall.container.find('.brick');
+    images.find('img').load(function() {
+      wall.fitWidth();
+    });
+  });
 });
